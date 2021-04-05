b425d51702bc54a099ce2a7bf1a9c6b673f24da8
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-04-04 21:29:02 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,1 +1,16 @@
-# Copy and paste the template here and then modify it as desired.
+# @desc Add a short description or instruction here. This will show up at the top of the exercise.
+
+def function_name(parameter):     # give your function a name and parameter(s)
+    # have it do stuff
+    return    # what does it return? This will be what the user types when they predict the result.
+
+
+def main():
+    print(function_name(parameter1))
+    print(function_name(parameter2))
+    print(function_name(parameter3))
+    print(function_name(parameter4))
+
+
+if __name__ == '__main__':
+    main()
