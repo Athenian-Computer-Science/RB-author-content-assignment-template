1a5cef615e3e61a6d862a9d10533d57cb0fa2729
Megan Leich <51799170+maleich@users.noreply.github.com>
2021-04-10 15:31:29 -0700

    Create CB-template.py

---
+++ CB-template.py
@@ -0,0 +1,16 @@
+# Hint: (What hint would you like to give when the user clicks the "Show hint" button?
+
+def function_name():    #choose a useful name for your function and give it as many parameters as you wish
+  
+  return some_variable      #choose what you are returning (you need to return something, not print)
+
+def main():
+  print(function_name(arg1))    #choose at least 5 test cases to run (preferably 10 or so).   
+  print(function_name(arg2))
+  print(function_name(arg3))    # Think about interesting things to test - where a value changes behavior,
+  print(function_name(arg4))    #  or where things might not behave as expected.
+  print(function_name(arg5))
+ 
+
+if __name__ == '__main__':
+    main()
